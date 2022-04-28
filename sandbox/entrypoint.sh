692c4a75df5f44b8bf60ba5a23b1176679de87b6
sgershuny <72019925+sgershuny@users.noreply.github.com>
2022-04-28 11:40:24 +0100

    Update entrypoint.sh

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -2,7 +2,7 @@
 ./run-bitcoin.sh
 ./generate-block-bitcoin.sh
 ./run-clightning.sh
-sleep 0.5m # Give the time to c-lightning to sync the network
+sleep 20m # Give the time to c-lightning to sync the network
 cd code
 echo "Running test with Cargo"
 cargo test
